a38a7c2f918b2617cb0b7eb8d3d74a77ea07bda8
Johanne GG <johanne.gjedrem@icloud.com>
2024-10-05 23:38:29 +0200

    Add: Add eye icon to views cell by installing react-icons

--- prosjekt2/src/components/AllSongsComponents/AllSongsList.tsx
+++ prosjekt2/src/components/AllSongsComponents/AllSongsList.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { FaEye } from "react-icons/fa";
 import { fetchSongs, Song } from "../../utils/FetchMockData";
 import "./AllSongsList.css";
 
@@ -22,23 +23,28 @@ export function AllSongsList() {
 		loadData();
 	}, []);
 
-  const formatViews = (views: number): string => {
-    return new Intl.NumberFormat('en', { notation: 'compact', compactDisplay: 'short' }).format(views);
-  };
+	const formatViews = (views: number): string => {
+		return new Intl.NumberFormat("en", { notation: "compact", compactDisplay: "short" }).format(
+			views,
+		);
+	};
 
 	if (loading) return <p>Loading songs...</p>;
 	if (error) return <p>{error}</p>;
 
 	return (
 		<section className="songContainer">
-			<h3>All Songs</h3>
 			<table className="songTable">
 				{songs.map((song) => (
+					// TODO: Add link to each lyrics pace
 					<tr key={song.id} className="tableRow">
 						<td>{song.title}</td>
 						<td>{song.artist}</td>
 						<td>{song.year}</td>
-						<td className="viewsCell">{formatViews(song.views)}</td>
+						<td className="viewsCell">
+							<FaEye style={{ marginRight: "5px" }} />
+							{formatViews(song.views)}
+						</td>
 					</tr>
 				))}
 			</table>
