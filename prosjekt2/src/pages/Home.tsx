e676edd2a7e2734d1a30a5f01d5f378ce3888b1d
Julia Dai <juliada@stud.ntnu.no>
2024-10-06 16:50:17 +0200

    Remove: Remove routing examples in Home

--- prosjekt2/src/pages/Home.tsx
+++ prosjekt2/src/pages/Home.tsx
@@ -1,24 +1,10 @@
-import { useNavigate } from "react-router-dom";
 import { AllSongsList } from "../components/AllSongsComponents/AllSongsList";
 
 const Home = () => {
-    const navigate = useNavigate();
-
-    const routeChange = (artistName: string, songTitle: string) => {
-        const path = `/${artistName.toLowerCase().replace(/ /g, "-")}/${songTitle.toLowerCase().replace(/ /g, "-")}`;
-        navigate(path);
-    };
 
     return (
         <>
             <AllSongsList/>
-            {/* Example buttons to navigate to different songs */}
-            <button onClick={() => routeChange("Ed Sheeran", "Perfect")}>
-                Perfect by Ed Sheeran
-            </button>
-            <button onClick={() => routeChange("Ed Sheeran", "Shape of You")}>
-                Shape of You by Ed Sheeran
-            </button>
         </>
     );
 };
