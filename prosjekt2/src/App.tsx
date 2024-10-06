ebeaae56a75baa485c1ec8bd5305836be3b47b76
Anna Farestrand <annatfa@stud.ntnu.no>
2024-10-06 13:13:33 +0200

    Styling: Add container for components

--- prosjekt2/src/App.tsx
+++ prosjekt2/src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
 import "./App.css";
 import { AllSongsList } from "./components/AllSongsComponents/AllSongsList";
 import Header from "./components/Header/Header";
 
+import "./components/Filter/Filter";
+import Filter from "./components/Filter/Filter";
+
 function App() {
+
 
 	return (
 		<>
-			<Header></Header>
-			<AllSongsList />
+			<div className="appContainer">
+				<Header />
+				<Filter />
+				<AllSongsList />
+			</div>
 		</>
 	);
 }
