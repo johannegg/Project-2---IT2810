fb016a224967320f50c8a12796f841dd68b92608
Anniken Strand <annikeqs@stud.ntnu.no>
2024-10-06 12:38:05 +0200

    Fix: Change component name from lower case to upper case

--- prosjekt2/src/App.tsx
+++ prosjekt2/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { AllSongsList } from "./components/AllSongsComponents/AllSongsList";
-import Header from "./components/header/header.tsx";
+import Header from "./components/Header/Header";
 
 function App() {
 
