2a3737c6d1d736a8803d934a63211343b474a2fa
Johanne GG <johanne.gjedrem@icloud.com>
2024-10-08 01:09:57 +0200

    Add: Create a basic search bar

---
+++ prosjekt2/frontend/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,12 @@
+import { FaSearch } from "react-icons/fa";
+import "./SearchBar.css";
+export function SearchBar() {
+	return (
+		<div className="searchContainer">
+			<input className="searchInput" placeholder="Search after a song or an artist" />
+			<button type="submit" className="searchButton">
+				<FaSearch className="searchIcon"/>
+			</button>
+		</div>
+	);
+}
