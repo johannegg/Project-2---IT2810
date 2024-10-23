ca571ea8813dc1720d8ac9fda06971822e1dddc0
Johanne GG <johanne.gjedrem@icloud.com>
2024-10-23 19:40:11 +0200

    Fix: Keep slash on filterbutton even on smaller screens

--- prosjekt2/frontend/src/components/SideBar/FilterButton/FilterButton.tsx
+++ prosjekt2/frontend/src/components/SideBar/FilterButton/FilterButton.tsx
@@ -7,7 +7,7 @@ type filterButtonProps = {
 export function FilterButton({ onClick }: filterButtonProps) {
 	return (
 		<button className="filterButton" onClick={onClick} type="button">
-			<FaFilter className="filterSortIcon" /> <span className="buttonText">Filter /</span>{" "}
+			<FaFilter className="filterSortIcon" /> <span className="buttonText">Filter</span><span className="filterSlash"> &nbsp; /</span>{" "}
 			<FaSort className="filterSortIcon sortIcon" /> <span className="buttonText">Sort</span>
 		</button>
 	);
