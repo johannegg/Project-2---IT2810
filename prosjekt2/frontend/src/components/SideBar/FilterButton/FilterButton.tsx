45f1ab956406b86c95a1e77cbb16fd1a50240ec8
Johanne GG <johanne.gjedrem@icloud.com>
2024-10-23 18:49:52 +0200

    Format: Run prettier

--- prosjekt2/frontend/src/components/SideBar/FilterButton/FilterButton.tsx
+++ prosjekt2/frontend/src/components/SideBar/FilterButton/FilterButton.tsx
@@ -2,12 +2,13 @@ import { FaFilter, FaSort } from "react-icons/fa";
 import "./FilterButton.css";
 
 type filterButtonProps = {
-  onClick: () => void;
+	onClick: () => void;
 };
 export function FilterButton({ onClick }: filterButtonProps) {
 	return (
 		<button className="filterButton" onClick={onClick} type="button">
-      <FaFilter className="filterSortIcon"/> <span className="buttonText">Filter /</span> <FaSort className="filterSortIcon"/> <span className="buttonText">Sort</span>
-    </button>
+			<FaFilter className="filterSortIcon" /> <span className="buttonText">Filter /</span>{" "}
+			<FaSort className="filterSortIcon" /> <span className="buttonText">Sort</span>
+		</button>
 	);
 }
