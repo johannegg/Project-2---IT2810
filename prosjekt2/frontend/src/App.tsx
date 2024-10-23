5716915f64ad37bcf0c95eecbde8fc4f8b166552
Anniken Strand <annikeqs@stud.ntnu.no>
2024-10-23 23:33:36 +0200

    Fix: Update imports i App.tsx

--- prosjekt2/frontend/src/App.tsx
+++ prosjekt2/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Favorites from "./pages/Favorites";
-import Playlists from "./pages/Playlists";
+import Playlists from "./pages/Playlists/Playlists";
 import "./App.css";
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
