faa38e454dda13570c9d1f51f7be1c04b5b32fe9
Anniken Strand <annikeqs@stud.ntnu.no>
2024-10-24 20:05:47 +0200

    Refactor: Reuse allsongs component when displaying playlist content

--- prosjekt2/frontend/src/components/DisplayPlaylist/DisplayPlaylist.tsx
+++ prosjekt2/frontend/src/components/DisplayPlaylist/DisplayPlaylist.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { PlaylistData } from '../../pages/Playlists/Playlists'; 
 import './DisplayPlaylist.css';
+import { AllSongsList } from '../AllSongsComponents/AllSongsList';
+
 
 interface DisplayPlaylistProps {
     playlist: PlaylistData;
@@ -8,23 +10,14 @@ interface DisplayPlaylistProps {
 
 const DisplayPlaylist: React.FC<DisplayPlaylistProps> = ({ playlist }) => {
     return (
-        <div className="playlist-details">
+        <section className="playlist-details">
             <h1>{playlist.name}</h1>
-            <section className="songs-list">
-                <h2>Songs in this playlist</h2>
                 {playlist.songs.length > 0 ? (
-                    <ul>
-                        {playlist.songs.map((song) => (
-                            <li key={song.id}>
-                                {song.title} by {song.artist}
-                            </li>
-                        ))}
-                    </ul>
+                    <AllSongsList songs={playlist.songs} genres={[]} /> 
                 ) : (
                     <p>No songs available</p>
                 )}
-            </section>
-        </div>
+        </section>
     );
 };
 
