afd441bb9ef6bfb96385b5509b43ac3edcd61115
Julia Dai <juliada@stud.ntnu.no>
2024-10-26 12:21:19 +0200

    Fix: Run prettier in backend

--- prosjekt2/backend/src/graphql/typeDefs.ts
+++ prosjekt2/backend/src/graphql/typeDefs.ts
@@ -1,35 +1,34 @@
-import { gql } from 'apollo-server-express';
+import { gql } from "apollo-server-express";
 
-export const typeDefs = gql `
+export const typeDefs = gql`
   type Song {
-  id: ID!
-  title: String!
-  year: Int
-  lyrics: String
-  views: Int
-  artist: Artist
-  genre: Genre
-}
+    id: ID!
+    title: String!
+    year: Int
+    lyrics: String
+    views: Int
+    artist: Artist
+    genre: Genre
+  }
 
-type Artist {
-  id: ID!
-  name: String!
-  songs: [Song]
-}
+  type Artist {
+    id: ID!
+    name: String!
+    songs: [Song]
+  }
 
-type Genre {
-  id: ID!
-  name: String!
-  songs: [Song]
-}
+  type Genre {
+    id: ID!
+    name: String!
+    songs: [Song]
+  }
 
-type Query {
-  songs: [Song]
-  song(id: ID!): Song
-  artists: [Artist]
-  artist(id: ID!): Artist
-  genres: [Genre]
-  genre(id: ID!): Genre
-}
-`
-;
+  type Query {
+    songs: [Song]
+    song(id: ID!): Song
+    artists: [Artist]
+    artist(id: ID!): Artist
+    genres: [Genre]
+    genre(id: ID!): Genre
+  }
+`;
--- prosjekt2/backend/src/index.ts
+++ prosjekt2/backend/src/index.ts
@@ -1,19 +1,20 @@
-import { ApolloServer } from 'apollo-server';
-import { Neo4jGraphQL } from '@neo4j/graphql';
-import neo4j from 'neo4j-driver';
-import dotenv from 'dotenv';
-import { typeDefs } from './graphql/typeDefs';
-import { resolvers } from './graphql/resolvers';
-
+import { ApolloServer } from "apollo-server";
+import { Neo4jGraphQL } from "@neo4j/graphql";
+import neo4j from "neo4j-driver";
+import dotenv from "dotenv";
+import { typeDefs } from "./graphql/typeDefs";
+import { resolvers } from "./graphql/resolvers";
 
 dotenv.config();
 
 // Create Neo4j driver
 const driver = neo4j.driver(
   process.env.NEO4J_URI as string,
-  neo4j.auth.basic(process.env.NEO4J_USER as string, process.env.NEO4J_PASSWORD as string)
+  neo4j.auth.basic(
+    process.env.NEO4J_USER as string,
+    process.env.NEO4J_PASSWORD as string,
+  ),
 );
-
 
 // Create Neo4j GraphQL schema
 const neoSchema = new Neo4jGraphQL({ typeDefs, driver, resolvers });
@@ -29,13 +30,16 @@ async function startApolloServer() {
       },
     });
 
-    server.listen().then(({ url }) => {
-      console.log(`🚀 Server ready at: ${url}`);
-    }).catch((error) => {
-      console.error('Error starting the Apollo server:', error);
-    });
+    server
+      .listen()
+      .then(({ url }) => {
+        console.log(`🚀 Server ready at: ${url}`);
+      })
+      .catch((error) => {
+        console.error("Error starting the Apollo server:", error);
+      });
   } catch (error) {
-    console.error('Error initializing the Neo4j GraphQL schema:', error);
+    console.error("Error initializing the Neo4j GraphQL schema:", error);
   }
 }
 
