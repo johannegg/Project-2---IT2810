26939eb6d3d71b06c11bb497e105be849315e86a
Julia Dai <juliada@stud.ntnu.no>
2024-10-26 12:27:12 +0200

    Refactor: Use Neo4j driver from utils folder

--- prosjekt2/backend/src/index.ts
+++ prosjekt2/backend/src/index.ts
@@ -1,20 +1,11 @@
-import { ApolloServer } from "apollo-server";
-import { Neo4jGraphQL } from "@neo4j/graphql";
-import neo4j from "neo4j-driver";
-import dotenv from "dotenv";
-import { typeDefs } from "./graphql/typeDefs";
-import { resolvers } from "./graphql/resolvers";
-
+import dotenv from 'dotenv';
 dotenv.config();
 
-// Create Neo4j driver
-const driver = neo4j.driver(
-  process.env.NEO4J_URI as string,
-  neo4j.auth.basic(
-    process.env.NEO4J_USER as string,
-    process.env.NEO4J_PASSWORD as string,
-  ),
-);
+import { ApolloServer } from 'apollo-server';
+import { Neo4jGraphQL } from '@neo4j/graphql';
+import { typeDefs } from './graphql/typeDefs';
+import { resolvers } from './graphql/resolvers';
+import driver from './utils/neo4j-driver';
 
 // Create Neo4j GraphQL schema
 const neoSchema = new Neo4jGraphQL({ typeDefs, driver, resolvers });
@@ -30,16 +21,13 @@ async function startApolloServer() {
       },
     });
 
-    server
-      .listen()
-      .then(({ url }) => {
-        console.log(`🚀 Server ready at: ${url}`);
-      })
-      .catch((error) => {
-        console.error("Error starting the Apollo server:", error);
-      });
+    server.listen().then(({ url }) => {
+      console.log(`🚀 Server ready at: ${url}`);
+    }).catch((error) => {
+      console.error('Error starting the Apollo server:', error);
+    });
   } catch (error) {
-    console.error("Error initializing the Neo4j GraphQL schema:", error);
+    console.error('Error initializing the Neo4j GraphQL schema:', error);
   }
 }
 
--- prosjekt2/backend/src/utils/neo4j-driver.ts
+++ prosjekt2/backend/src/utils/neo4j-driver.ts
@@ -1,5 +1,6 @@
 import neo4j from "neo4j-driver";
 
+// Create Neo4j driver
 const driver = neo4j.driver(
   process.env.NEO4J_URI as string,
   neo4j.auth.basic(
