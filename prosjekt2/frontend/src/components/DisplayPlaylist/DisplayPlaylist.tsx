5c9749c42fafa6e3e87363855a14d2c7815a57bf
Anniken Strand <annikeqs@stud.ntnu.no>
2024-10-27 17:55:51 +0100

    Styling: Fix layout for page that shows a playlist

--- prosjekt2/frontend/src/components/DisplayPlaylist/DisplayPlaylist.tsx
+++ prosjekt2/frontend/src/components/DisplayPlaylist/DisplayPlaylist.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import { PlaylistData } from '../../pages/Playlists/Playlists'; 
-import './DisplayPlaylist.css';
-import { AllSongsList } from '../AllSongsComponents/AllSongsList';
-
+import React from "react";
+import { PlaylistData } from "../../pages/Playlists/Playlists";
+import "./DisplayPlaylist.css";
+import { AllSongsList } from "../AllSongsComponents/AllSongsList";
 
 interface DisplayPlaylistProps {
-    playlist: PlaylistData;
+	playlist: PlaylistData;
 }
 
 const DisplayPlaylist: React.FC<DisplayPlaylistProps> = ({ playlist }) => {
-    return (
-        <section className="playlist-details">
-            <h1>{playlist.name + " "+ playlist.icon}</h1>
-                {playlist.songs.length > 0 ? (
-                    <AllSongsList songs={playlist.songs} genres={[]} /> 
-                ) : (
-                    <p>No songs available</p>
-                )}
-        </section>
-    );
+	return (
+		<section className="playlist-details">
+			<h1>{playlist.name + " " + playlist.icon}</h1>
+			<div className="songs-container">
+				{playlist.songs.length > 0 ? (
+					<AllSongsList songs={playlist.songs} genres={[]} />
+				) : (
+					<p>No songs available</p>
+				)}
+			</div>
+		</section>
+	);
 };
 
 export default DisplayPlaylist;
