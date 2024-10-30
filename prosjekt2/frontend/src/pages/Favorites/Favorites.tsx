bde565ab2eb3c9b1a3b1f6ccd4954554f4c5b125
Julia Dai <juliada@stud.ntnu.no>
2024-10-30 19:51:20 +0100

    Fix: Reuse old searching function on Favorites page

--- prosjekt2/frontend/src/pages/Favorites/Favorites.tsx
+++ prosjekt2/frontend/src/pages/Favorites/Favorites.tsx
@@ -8,14 +8,35 @@ import { SongData } from "../../utils/types/SongTypes";
 const Favorites: React.FC = () => {
   const [favorites, setFavorites] = useState<SongData[]>([]);
   const [searchedSongs, setSearchedSongs] = useState<SongData[]>([]);
-  const genres = ["pop", "rock", "rap", "country"];
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const genres = ["pop", "rock", "rap", "rb", "country"];
   const navigate = useNavigate()
 
   useEffect(() => {
     const favoriteSongs = JSON.parse(localStorage.getItem("favoriteSongs") || "[]");
-    console.log("Favorite Songs:", favoriteSongs); 
     setFavorites(favoriteSongs);
+    setSearchedSongs(favoriteSongs)
   }, []);
+
+  // Filter favorite songs based on search term
+  useEffect(() => {
+		const filterSongs = () => {
+			if (searchTerm === "") return favorites;
+			const keywords = searchTerm.toLowerCase().split(" ");
+
+			return favorites.filter((song) => {
+				return keywords.every((keyword) => 
+					song.title.toLowerCase().includes(keyword) ||
+					song.artist.name.toLowerCase().includes(keyword)	
+				);
+			});
+		}
+		setSearchedSongs(filterSongs());
+	}, [searchTerm, favorites]);
+
+  const handleSearchSubmit = (term: string) => {
+		setSearchTerm(term); // Updates search term based on button click
+	};
 
   
   // Show message if no favorite songs are found
@@ -35,7 +56,7 @@ const Favorites: React.FC = () => {
     <section className="favorites-container">
       <h1>Your Favorite Songs</h1>
       <section className="favorites-searchBarContainer">
-        <SearchBar songs={favorites} setSearchedSongs={setSearchedSongs} />
+        <SearchBar setSearchTerm={handleSearchSubmit} />
       </section>
       <section className="favorites-allSongsContainer">
         <AllSongsList songs={searchedSongs} genres={genres} />
