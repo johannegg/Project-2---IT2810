537626950d61df0240b1e941ed7fe1bfe9556791
Julia Dai <juliada@stud.ntnu.no>
2024-10-30 19:32:59 +0100

    Add: Add clear search button

--- prosjekt2/frontend/src/components/SearchBar/SearchBar.tsx
+++ prosjekt2/frontend/src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,8 @@
 import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 type SearchBarProps = {
 	setSearchTerm: (setSearchTerm: string) => void;
@@ -8,11 +10,24 @@ type SearchBarProps = {
 
 export function SearchBar({ setSearchTerm }: SearchBarProps) {
 	const [searchInput, setSearchInput] = useState<string>("");
+	const [hasInput, setHasInput] = useState<boolean>(false);
 
 	const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault(); 
 		setSearchTerm(searchInput); // Pass search input to Home page
 	};
+
+	const handleSearchInput = (input: string) => {
+		setSearchInput(input);
+		if (input == "") setHasInput(false);
+		else setHasInput(true)
+	}
+
+	const clearInput = () => {
+		setSearchInput("");
+		setSearchTerm("");
+		setHasInput(false);
+	}
 
 	return (
 		<div className="searchContainer">
@@ -21,12 +36,17 @@ export function SearchBar({ setSearchTerm }: SearchBarProps) {
 					className="searchInput"
 					placeholder="Search for a song or an artist"
 					value={searchInput}
-					onChange={(e) => setSearchInput(e.target.value)}
+					onChange={(e) => handleSearchInput(e.target.value)}
 				/>
 				<button type="submit" className="iconContainer">
 					<FaSearch className="searchIcon" />
 				</button>
 			</form>
+			{hasInput &&
+				<button className="clearButton" onClick={clearInput}>
+					<FontAwesomeIcon icon={faXmark} />
+				</button>
+			}
 		</div>
 	);
 }
