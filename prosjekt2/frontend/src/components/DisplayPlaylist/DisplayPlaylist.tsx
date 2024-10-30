13bf0b3610de55a6bceae1e0651840352a557fd7
Anniken Strand <annikeqs@stud.ntnu.no>
2024-10-30 14:33:44 +0100

    Add: Make confirmation modal for playlist deletion

--- prosjekt2/frontend/src/components/DisplayPlaylist/DisplayPlaylist.tsx
+++ prosjekt2/frontend/src/components/DisplayPlaylist/DisplayPlaylist.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { PlaylistData } from "../../pages/Playlists/Playlists";
 import "./DisplayPlaylist.css";
 import { AllSongsList } from "../AllSongsComponents/AllSongsList";
@@ -9,9 +9,24 @@ interface DisplayPlaylistProps {
 }
 
 const DisplayPlaylist: React.FC<DisplayPlaylistProps> = ({ playlist, onDelete }) => {
+	const [showConfirmDelete, setShowConfirmDelete] = useState(false);
+
+	const handleDeleteClick = () => {
+		setShowConfirmDelete(true);
+	};
+
+	const handleConfirmDelete = () => {
+		onDelete();
+		setShowConfirmDelete(false);
+	};
+
+	const handleCancelDelete = () => {
+		setShowConfirmDelete(false);
+	};
+
 	return (
 		<section className="playlist-details">
-			<button onClick={onDelete} className="delete-button">
+			<button onClick={handleDeleteClick} className="delete-button">
 				Delete Playlist
 			</button>
 			<h1>{playlist.name + " " + playlist.icon}</h1>
@@ -22,6 +37,22 @@ const DisplayPlaylist: React.FC<DisplayPlaylistProps> = ({ playlist, onDelete })
 					<p>No songs here yet.</p>
 				)}
 			</div>
+
+			{showConfirmDelete && (
+				<div className="modal-overlay">
+					<div className="modal">
+						<p>Are you sure you want to delete this playlist?</p>
+						<div className="modal-buttons">
+							<button onClick={handleCancelDelete} className="cancel-button">
+								No
+							</button>
+							<button onClick={handleConfirmDelete} className="confirm-button">
+								Yes
+							</button>
+						</div>
+					</div>
+				</div>
+			)}
 		</section>
 	);
 };
