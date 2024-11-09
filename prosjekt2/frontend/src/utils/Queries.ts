13fc0c954001653cf507c9ffa3f3fc7da804ba5d
Julia Dai <juliada@stud.ntnu.no>
2024-11-09 17:41:58 +0100

    Add: Add GraphQL mutations for User

--- prosjekt2/frontend/src/utils/Queries.ts
+++ prosjekt2/frontend/src/utils/Queries.ts
@@ -1,20 +1,67 @@
 import { gql } from '@apollo/client';
 
 export const GET_SONGS = gql`
-    query GetSongs($skip: Int, $limit: Int, $genres: [String], $sortBy: SortBy, $searchTerm: String) {
-        songs(skip: $skip, limit: $limit, genres: $genres, sortBy: $sortBy, searchTerm: $searchTerm) {
-            id
-            title
-            views
-            year
-            lyrics
-            artist {
-                name
-                id
-            }
-            genre {
-                name
-            }
-        }
+  query GetSongs($skip: Int, $limit: Int, $genres: [String], $sortBy: SortBy, $searchTerm: String) {
+    songs(skip: $skip, limit: $limit, genres: $genres, sortBy: $sortBy, searchTerm: $searchTerm) {
+      id
+      title
+      views
+      year
+      lyrics
+      artist {
+        name
+        id
+      }
+      genre {
+        name
+      }
     }
+  }
 `;
+
+export const CREATE_USER = gql`
+  mutation CreateUser($username: String) {
+    createUser(username: $username) {
+      id
+      username
+      favoriteSongs {
+        id
+        title
+        views
+        year
+        lyrics
+        artist {
+          name
+        }
+        genre {
+          name
+        }
+      }
+      playlists {
+        id
+        name
+        songs {
+          id
+          title
+          views
+          year
+          lyrics
+          artist {
+            name
+            id
+          }
+          genre {
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const DELETE_USER = gql`
+  mutation DeleteUser($username: String!) {
+    deleteUser(username: $username)  
+  }
+            
+`
