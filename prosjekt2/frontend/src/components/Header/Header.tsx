932dd70a76548ec00d15b91edca5b19f9fa92541
Julia Dai <juliada@stud.ntnu.no>
2024-11-13 12:55:26 +0100

    Add: Add alert when not logged in

--- prosjekt2/frontend/src/components/Header/Header.tsx
+++ prosjekt2/frontend/src/components/Header/Header.tsx
@@ -1,12 +1,31 @@
 import "./Header.css";
 import couchIcon from "../../assets/couch.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { BsMusicNoteList } from "react-icons/bs";
 import Profile from "../Profile/Profile";
+import { useState, useEffect } from "react";
 
 const Header: React.FC = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const username = localStorage.getItem("profileName");
+		if (username && username !== "") {
+			setIsLoggedIn(true);
+		}
+	}, []);
+
+	const handleNavigation = (path: string) => {
+		if (isLoggedIn) {
+			navigate(path);
+		} else {
+			alert("You need to log in to access this page.");
+		}
+	};
+
 	return (
 		<header className="header">
 			<Link to="/">
@@ -16,24 +35,45 @@ const Header: React.FC = () => {
 				</div>
 			</Link>
 			<nav className="navbar">
-				<Link to="/favorites">
-					<button
-						type="button"
-						className={`navButton ${location.pathname === "/favorites" ? "active" : ""}`}
-					>
-						<FontAwesomeIcon icon={faHeart} size="2x" className="navIcon" />
-						<span className="navText">Favorited songs</span>
-					</button>
-				</Link>
-				<Link to="/playlists">
-					<button
-						type="button"
-						className={`navButton ${location.pathname === "/playlists" ? "active" : ""}`}
-					>
-						<BsMusicNoteList className="navIcon musicIcon" />
-						<span className="navText">Your playlists</span>
-					</button>
-				</Link>
+				{isLoggedIn ? (
+					<>
+						<button
+							type="button"
+							onClick={() => handleNavigation("/favorites")}
+							className={`navButton ${location.pathname === "/favorites" ? "active" : ""}`}
+						>
+							<FontAwesomeIcon icon={faHeart} size="2x" className="navIcon" />
+							<span className="navText">Favorited songs</span>
+						</button>
+						<button
+							type="button"
+							onClick={() => handleNavigation("/playlists")}
+							className={`navButton ${location.pathname === "/playlists" ? "active" : ""}`}
+						>
+							<BsMusicNoteList className="navIcon musicIcon" />
+							<span className="navText">Your playlists</span>
+						</button>
+					</>
+				) : (
+					<>
+						<button
+							type="button"
+							onClick={() => alert("You need to log in to view your favorited songs.")}
+							className={`navButton ${location.pathname === "/favorites" ? "active" : ""}`}
+						>
+							<FontAwesomeIcon icon={faHeart} size="2x" className="navIcon" />
+							<span className="navText">Favorited songs</span>
+						</button>
+						<button
+							type="button"
+							onClick={() => alert("You need to log in to view your playlists.")}
+							className={`navButton ${location.pathname === "/playlists" ? "active" : ""}`}
+						>
+							<BsMusicNoteList className="navIcon musicIcon" />
+							<span className="navText">Your playlists</span>
+						</button>
+					</>
+				)}
 				<Profile />
 			</nav>
 		</header>
