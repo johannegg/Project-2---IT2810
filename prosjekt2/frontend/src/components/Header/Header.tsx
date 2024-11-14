aad2897b36cd995687c4d9fd3e53af225bd3987d
Anna Farestrand <annatfa@stud.ntnu.no>
2024-11-14 16:12:33 +0100

    Fix: Remove button inside Link

--- prosjekt2/frontend/src/components/Header/Header.tsx
+++ prosjekt2/frontend/src/components/Header/Header.tsx
@@ -6,32 +6,29 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { BsMusicNoteList } from "react-icons/bs";
 
 const Header: React.FC = () => {
+
 	return (
 		<header className="header">
 			<Link to="/">
-				<div className="header-content">
+				<a className="header-content">	
 					<img src={couchIcon} alt="Sofa Icon" className="sofa-icon" />
 					<h1 className="header-title">Lyrical Lounge</h1>
-				</div>
+				</a>
 			</Link>
 			<nav className="navbar">
-				<Link to="/favorites">
-					<button
-						type="button"
-						className={`navButton ${location.pathname === "/favorites" ? "active" : ""}`}
-					>
-						<FontAwesomeIcon icon={faHeart} size="2x" className="navIcon" />
-						<span className="navText">Favorited songs</span>
-					</button>
+				<Link
+					to="/favorites"
+					className={`navButton ${location.pathname === "/favorites" ? "active" : ""}`}
+				>
+					<FontAwesomeIcon icon={faHeart} size="2x" className="navIcon" />
+					<span className="navText">Favorited songs</span>
 				</Link>
-				<Link to="/playlists">
-					<button
-						type="button"
-						className={`navButton ${location.pathname === "/playlists" ? "active" : ""}`}
-					>
-						<BsMusicNoteList className="navIcon musicIcon" />
-						<span className="navText">Your playlists</span>
-					</button>
+				<Link
+					to="/playlists"
+					className={`navButton ${location.pathname === "/playlists" ? "active" : ""}`}
+				>
+					<BsMusicNoteList className="navIcon musicIcon" />
+					<span className="navText">Your playlists</span>
 				</Link>
 			</nav>
 		</header>
