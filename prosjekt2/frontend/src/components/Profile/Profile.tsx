95d90bd91029b28a6378bef10aad246132dcf782
Julia Dai <juliada@stud.ntnu.no>
2024-11-18 14:37:23 +0100

    Remove: Unused usertaken check in profile

--- prosjekt2/frontend/src/components/Profile/Profile.tsx
+++ prosjekt2/frontend/src/components/Profile/Profile.tsx
@@ -12,12 +12,10 @@ const Profile: React.FC = () => {
 	const [isLoggedIn, setLogin] = useState<boolean>(false);
 	const [inputValue, setInputValue] = useState<string>("");
 	const [showLogin, setShowLogin] = useState<boolean>(false);
-	const [usernameTaken, setUsernameTaken] = useState<boolean>(false);
-	
 	// Initialize the createUser mutation
 	const [createUser, { loading: creatingUser, error: userError }] = useMutation(CREATE_USER);
-	
-	const navigate = useNavigate()
+
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		const storedProfileName = localStorage.getItem("profileName");
@@ -30,7 +28,7 @@ const Profile: React.FC = () => {
 	const logOut = () => {
 		setLogin(false);
 		localStorage.removeItem("profileName");
-		localStorage.removeItem("favoriteSongs")
+		localStorage.removeItem("favoriteSongs");
 		localStorage.removeItem("playlists");
 		playlistsVar([]);
 		favoriteSongsVar([]);
@@ -45,7 +43,6 @@ const Profile: React.FC = () => {
 				if (data && data.createUser) {
 					// If user creation is successful
 					setProfile(data.createUser.username);
-					setUsernameTaken(false);
 					setLogin(true);
 					favoriteSongsVar(data.createUser.favoriteSongs);
 					localStorage.setItem("profileName", data.createUser.username);
@@ -78,73 +75,43 @@ const Profile: React.FC = () => {
 					</div>
 					{showLogin && (
 						<div className="login-overlay">
-						<FontAwesomeIcon
-							className="close-login-button"
-							icon={faXmark}
-							onClick={() => setShowLogin(false)}
-						/>
-						<div className="profile-login">
-							{usernameTaken ? (
-								<>
-									<p>This username is already taken. Do you want to proceed?</p>
-									<div className="modal-buttons">
-										<button
-											className="cancel-button"
-											onClick={() => {
-												setUsernameTaken(false);
-												setInputValue(""); // Reset input value for a new attempt
-											}}
-										>
-											No
-										</button>
-										<button
-											className="confirm-button"
-											onClick={() => {
-												setShowLogin(false);
-												setUsernameTaken(false);
-											}}
-										>
-											Yes
-										</button>
-									</div>
-								</>
-							) : (
-								<>
-									<p className="login-information">
-										{creatingUser
-											? "Logging in..."
-											: "Choose a unique username, or log in with an existing one"}
-									</p>
-									{userError && (
-										<p className="error-message">{userError.message}</p>
-									)}
-									<form
-										className="login-form"
-										onSubmit={(e) => {
-											e.preventDefault();
-											logIn();
-										}}
-									>
-										<FontAwesomeIcon
-											className="profile-login-icon"
-											icon={faCircleUser}
-											size="lg"
-											style={{ color: "#ea9ab2" }}
-										/>
-										<input
-											className="login-input"
-											placeholder="Enter username"
-											value={inputValue}
-											onChange={(e) => setInputValue(e.target.value)}
-										/>
-										<button className="login-button" type="submit">
-											<FontAwesomeIcon icon={faArrowRight} style={{ color: "#FFF" }} />
-										</button>
-									</form>
-								</>
-							)}
+							<FontAwesomeIcon
+								className="close-login-button"
+								icon={faXmark}
+								onClick={() => setShowLogin(false)}
+							/>
+							<div className="profile-login">
+								<p className="login-information">
+									{creatingUser
+										? "Logging in..."
+										: "Choose a unique username, or log in with an existing one"}
+								</p>
+								{userError && <p className="error-message">{userError.message}</p>}
+								<form
+									className="login-form"
+									onSubmit={(e) => {
+										e.preventDefault();
+										logIn();
+									}}
+								>
+									<FontAwesomeIcon
+										className="profile-login-icon"
+										icon={faCircleUser}
+										size="lg"
+										style={{ color: "#ea9ab2" }}
+									/>
+									<input
+										className="login-input"
+										placeholder="Enter username"
+										value={inputValue}
+										onChange={(e) => setInputValue(e.target.value)}
+									/>
+									<button className="login-button" type="submit">
+										<FontAwesomeIcon icon={faArrowRight} style={{ color: "#FFF" }} />
+									</button>
+								</form>
+							</div>
 						</div>
-					</div>
 					)}
 				</>
 			)}
@@ -153,5 +120,6 @@ const Profile: React.FC = () => {
 };
 
 export default Profile;
+
 
 
