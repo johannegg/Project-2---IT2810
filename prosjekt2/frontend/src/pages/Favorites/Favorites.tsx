f403be5b0bbc419ea290277e52531e2c59ea1644
Johanne GG <johanne.gjedrem@icloud.com>
2024-11-18 15:04:56 +0100

    Remove: Delete unnecessary genres variable

--- prosjekt2/frontend/src/pages/Favorites/Favorites.tsx
+++ prosjekt2/frontend/src/pages/Favorites/Favorites.tsx
@@ -12,7 +12,6 @@ const Favorites: React.FC = () => {
 	const favorites = useReactiveVar(favoriteSongsVar);
 	const searchTerm = useReactiveVar(favoritesSearchTermVar);
 	const [searchedSongs, setSearchedSongs] = useState<SongData[]>(favorites);
-	const genres = ["pop", "rock", "rap", "rb", "country"];
 	const navigate = useNavigate();
 
 	useEffect(() => {
