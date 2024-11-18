26f4ea3766b843968afefbfde7fc0b21da2938e0
Anniken Strand <annikeqs@stud.ntnu.no>
2024-11-18 14:05:17 +0100

    Format: Run prettier

--- prosjekt2/frontend/src/components/Profile/Profile.tsx
+++ prosjekt2/frontend/src/components/Profile/Profile.tsx
@@ -68,73 +68,71 @@ const Profile: React.FC = () => {
 					</div>
 					{showLogin && (
 						<div className="login-overlay">
-						<FontAwesomeIcon
-							className="close-login-button"
-							icon={faXmark}
-							onClick={() => setShowLogin(false)}
-						/>
-						<div className="profile-login">
-							{usernameTaken ? (
-								<>
-									<p>This username is already taken. Do you want to proceed?</p>
-									<div className="modal-buttons">
-										<button
-											className="cancel-button"
-											onClick={() => {
-												setUsernameTaken(false);
-												setInputValue(""); // Reset input value for a new attempt
+							<FontAwesomeIcon
+								className="close-login-button"
+								icon={faXmark}
+								onClick={() => setShowLogin(false)}
+							/>
+							<div className="profile-login">
+								{usernameTaken ? (
+									<>
+										<p>This username is already taken. Do you want to proceed?</p>
+										<div className="modal-buttons">
+											<button
+												className="cancel-button"
+												onClick={() => {
+													setUsernameTaken(false);
+													setInputValue(""); // Reset input value for a new attempt
+												}}
+											>
+												No
+											</button>
+											<button
+												className="confirm-button"
+												onClick={() => {
+													setShowLogin(false);
+													setUsernameTaken(false);
+												}}
+											>
+												Yes
+											</button>
+										</div>
+									</>
+								) : (
+									<>
+										<p className="login-information">
+											{creatingUser
+												? "Logging in..."
+												: "Choose a unique username, or log in with an existing one"}
+										</p>
+										{userError && <p className="error-message">{userError.message}</p>}
+										<form
+											className="login-form"
+											onSubmit={(e) => {
+												e.preventDefault();
+												logIn();
 											}}
 										>
-											No
-										</button>
-										<button
-											className="confirm-button"
-											onClick={() => {
-												setShowLogin(false);
-												setUsernameTaken(false);
-											}}
-										>
-											Yes
-										</button>
-									</div>
-								</>
-							) : (
-								<>
-									<p className="login-information">
-										{creatingUser
-											? "Logging in..."
-											: "Choose a unique username, or log in with an existing one"}
-									</p>
-									{userError && (
-										<p className="error-message">{userError.message}</p>
-									)}
-									<form
-										className="login-form"
-										onSubmit={(e) => {
-											e.preventDefault();
-											logIn();
-										}}
-									>
-										<FontAwesomeIcon
-											className="profile-login-icon"
-											icon={faCircleUser}
-											size="lg"
-											style={{ color: "#ea9ab2" }}
-										/>
-										<input
-											className="login-input"
-											placeholder="Enter username"
-											value={inputValue}
-											onChange={(e) => setInputValue(e.target.value)}
-										/>
-										<button className="login-button" type="submit">
-											<FontAwesomeIcon icon={faArrowRight} style={{ color: "#FFF" }} />
-										</button>
-									</form>
-								</>
-							)}
+											<FontAwesomeIcon
+												className="profile-login-icon"
+												icon={faCircleUser}
+												size="lg"
+												style={{ color: "#ea9ab2" }}
+											/>
+											<input
+												className="login-input"
+												placeholder="Enter username"
+												value={inputValue}
+												onChange={(e) => setInputValue(e.target.value)}
+											/>
+											<button className="login-button" type="submit">
+												<FontAwesomeIcon icon={faArrowRight} style={{ color: "#FFF" }} />
+											</button>
+										</form>
+									</>
+								)}
+							</div>
 						</div>
-					</div>
 					)}
 				</>
 			)}
@@ -143,5 +141,3 @@ const Profile: React.FC = () => {
 };
 
 export default Profile;
-
-
