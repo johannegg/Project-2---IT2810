fdb7843648ebf42e16f051c7d4f07a279730de4d
Anna Farestrand <annatfa@stud.ntnu.no>
2024-11-20 12:43:29 +0100

    Fix: Make project uploadable to VM

--- prosjekt2/frontend/src/apollo/client.ts
+++ prosjekt2/frontend/src/apollo/client.ts
@@ -2,7 +2,8 @@ import { ApolloClient } from "@apollo/client";
 import cache from "./cache";
 
 const client = new ApolloClient({
-	uri: "http://localhost:4000",
+	uri: "http://localhost:3001/", // Use this to run locally, for tests
+	//uri: "http://it2810-12.idi.ntnu.no:3001", // Use this to run on virtual machine
 	cache: cache,
 });
 
