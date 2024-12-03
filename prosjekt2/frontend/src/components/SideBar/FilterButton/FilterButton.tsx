f57b1636d4ec32d296aa161d733fa7818ca89b44
Anna Farestrand <annatfa@stud.ntnu.no>
2024-12-03 15:55:09 +0100

    Fix: Run prettier (again)

--- prosjekt2/frontend/src/components/SideBar/FilterButton/FilterButton.tsx
+++ prosjekt2/frontend/src/components/SideBar/FilterButton/FilterButton.tsx
@@ -8,7 +8,10 @@ export function FilterButton({ onClick }: filterButtonProps) {
 	return (
 		<button className="filterButton" onClick={onClick} type="button">
 			<FaFilter className="filterSortIcon" /> <span className="buttonText">Filter</span>
-			<span className="filterSlash"> &nbsp; /</span> <FaSort className="filterSortIcon sortIcon" />{" "}
+			<span className="filterSlash">
+				{" "}
+				&nbsp; /
+			</span> <FaSort className="filterSortIcon sortIcon" />{" "}
 			<span className="buttonText">Sort</span>
 		</button>
 	);
